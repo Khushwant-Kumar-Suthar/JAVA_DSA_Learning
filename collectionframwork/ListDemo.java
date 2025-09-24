e00848c5c2342108e3b63d1ab9fdd259a4f9a26e
Khushwant-Kumar-Suthar <khushwantsuthar49@gmail.com>
2025-09-24 18:14:45 +0530

    Day 2 commit

---
+++ collectionframwork/ListDemo.java
@@ -0,0 +1,26 @@
+package JAVA_DSA_Learning.collectionframwork;
+
+import java.util.*;
+
+public class ListDemo {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> al1 = new ArrayList<>();
+
+        ArrayList<Integer> al2 = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        al1.add(0, 11);
+        al1.add(12);
+        al1.addAll(al2);
+        System.out.println(al1.indexOf(12));
+        System.out.println(al1);
+        //al1.clear();
+        System.out.println(al1);
+        System.out.println(al1.isEmpty());
+        // there are many more methods in list interface
+        // you can explore them on your own
+
+        for(int num : al1){
+            System.out.println(num);
+        }
+    }
+}
