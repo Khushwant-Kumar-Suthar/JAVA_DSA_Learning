20e41d54e8577c005dc7c17276a7614eebfd485b
Khushwant-Kumar-Suthar <khushwantsuthar49@gmail.com>
2025-10-01 18:31:17 +0530

    Day 3

--- collectionframwork/TreeSetDemo.java
+++ collectionframwork/TreeSetDemo.java
@@ -3,6 +3,13 @@ import  java.util.*;
 
 public class TreeSetDemo {
    public static void main(String[] args){
-
+       TreeSet<Integer> ts = new TreeSet<>();
+       ts.add(10);
+       ts.add(5);
+       ts.add(8);
+       ts.addAll(List.of(30,20,40,10,50,45));
+       System.out.println("using ceiling this gave equal or nearest greater number: "+ts.ceiling(46));
+       // this is sorted value store
+       ts.forEach(System.out::println);
     }
 }
